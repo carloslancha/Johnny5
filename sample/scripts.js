b3a279e7618fc556e5c9bc0a43c63cf2b4735f3c
Carlos Lancha <carlos.lancha@rumbo.com>
2015-03-29 17:26:24 +0200

    Refactor sample

--- sample/scripts.js
+++ sample/scripts.js
@@ -18,7 +18,8 @@ var myConversation = {
       phrase: 'Great! Write your email!',
       answers: [ {
         answer: function ( input ) {
-          return input === 'email';
+          var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+          return re.test( input );
         },
         goTo: 4
       }, {
